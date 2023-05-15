a921ddbd0e27c61b358419a4ad277815a7f15779
Le Xuan Hieu <lexuanhieu20032003@gmail.com>
2023-05-15 11:29:56 +0700

    complete topic memory

--- OtherProjects/src/hust/soict/ict/garbage/GarbageCreator.java
+++ OtherProjects/src/hust/soict/ict/garbage/GarbageCreator.java
@@ -1,19 +1,27 @@
 package hust.soict.ict.garbage;
 
-public class GarbageCreator {
-	public static void createGarbage() {
-        try {
-            while (true) {
-                String str = new String("abc");  // Creates a garbage object
-                str = null;
-            }
-        } catch (OutOfMemoryError e) {
-            System.out.println("Out of memory! Garbage creation stopped.");
-        }
-    }
-	
-	public static void main(String[] args) {
-		createGarbage();
-	}
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+public class GarbageCreator {
+	public static void main(String[] args) {
+		String fileName = "src/hust/soict/ict/garbage/pip.exe";
+		byte[] inputBytes = { 0 };
+		long startTime, endTime;
+		
+		try {
+			inputBytes = Files.readAllBytes(Paths.get(fileName));
+			startTime = System.currentTimeMillis();
+			String outputString = "";
+			for (byte b : inputBytes) {
+				outputString += (char)b;
+			}
+			endTime = System.currentTimeMillis();
+			System.out.println(endTime - startTime);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
 }
